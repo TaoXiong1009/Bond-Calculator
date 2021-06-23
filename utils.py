6b7fe7fa5d5a99c64b79514ccca1ad092e2b2abc
TaoXiong1009 <79813308+TaoXiong1009@users.noreply.github.com>
2021-06-23 10:11:38 +0800

    Add files via upload

--- utils.py
+++ utils.py
@@ -1,17 +1,19 @@
 import QuantLib as ql
 import pandas as pd
 from WindPy import *
-import time
 import numpy as np
+import datetime
 
 w.start()
-today = time.strftime("%Y-%m-%d",time.localtime(time.time()))
+today = datetime.date.today().strftime('%Y-%m-%d')
+
 
 def get_basic_info(code: str) -> dict:
     # 获取左上部分信息
 
     info = w.wsd(code, "exch_city,exchange_cn,sec_type", "ED0D", today, "PriceAdj=CP")
     info = np.ravel(info.Data)
+    info2 = w.wss(code, "coupon").Data[0][0]
     basic_info = {'location': info[0],
                   'platform': info[1],
                   'quote_convention': '',
@@ -24,39 +26,42 @@ def get_basic_info(code: str) -> dict:
 def get_quote(code: str) -> pd.DataFrame:
     quote_df = pd.DataFrame(None, index=['code', 'clean', 'full', 'yield'],
                             columns=['IB', 'SH', 'SZ'])
+    code_ib = w.wsd(code, "relationCode", "ED0D", today, "exchangeType=NIB").Data[0][0]
+    quote_ib = w.wsq(code_ib, "rt_last_dp,rt_last_cp,rt_last_ytm").Data
+    quote_ib.insert(0, [code_ib])
+    quote_df['IB'] = pd.Series(np.ravel(quote_ib), index=quote_df.index)
 
-    quote_ib = w.wsd(code, "trade_code,cleanprice,dirtyprice,ytm_b", "ED0D", today,
-               "returnType=1;TradingCalendar=NIB")
-    quote_df['IB'] = pd.Series(np.ravel(quote_ib.Data), index = quote_df.index)
+    code_sh = w.wsd(code, "relationCode", "ED0D", today, "exchangeType=SSE").Data[0][0]
+    quote_sh = w.wsq(code_sh, "rt_last_dp,rt_last_cp,rt_last_ytm").Data
+    quote_sh.insert(0, [code_sh])
+    quote_df['SH'] = pd.Series(np.ravel(quote_sh), index=quote_df.index)
 
-    quote_sh = w.wsd(code, "trade_code,cleanprice,dirtyprice,ytm_b", "ED0D", today,
-                     "returnType=1;TradingCalendar=SHFE")
-    quote_df['SH'] = pd.Series(np.ravel(quote_sh.Data), index = quote_df.index)
-    quote_sz = w.wsd(code, "trade_code,cleanprice,dirtyprice,ytm_b", "ED0D", today,
-               "returnType=1;TradingCalendar=SZSE")
-    quote_df['SZ'] = pd.Series(np.ravel(quote_sz.Data), index = quote_df.index)
+    code_sz = w.wsd(code, "relationCode", "ED0D", today, "exchangeType=SZSE").Data[0][0]
+    quote_sz = w.wsq(code_sz, "rt_last_dp,rt_last_cp,rt_last_ytm").Data
+    quote_sz.insert(0, [code_sz])
+    quote_df['SZ'] = pd.Series(np.ravel(quote_sz), index=quote_df.index)
 
     return quote_df
 
 
 def get_issue_date(code: str) -> float:
-    pass
+    return w.wss(code, "issue_date").Data[0][0].strftime('%Y-%m-%d')
 
 
 def get_coupon_rate(code: str) -> float:
-    pass
+    return w.wss(code, "couponrate2").Data[0][0]
 
 
 def get_maturity_date(code: str) -> str:
-    pass
+    return w.wss(code, "maturitydate").Data[0][0].strftime('%Y-%m-%d')
 
 
 def get_tenor(code: str) -> int:
-    pass
+    return int(12/w.wss(code, "interestfrequency").Data[0][0])
 
 
 def get_accrural_method(code: str) -> float:
-    pass
+    return w.wss(code, "coupon").Data[0][0]
 
 
 def convert_accrural_method(accrural_method: str) -> ql.DayCounter:
